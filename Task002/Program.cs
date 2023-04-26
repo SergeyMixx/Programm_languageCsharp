80c66e1e81db4bbd82fee4aa7d3a8734132edeef
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-04-26 23:22:46 +0300

    Completed HomeWork task 2

---
+++ Task002/Program.cs
@@ -0,0 +1,8 @@
+﻿int max =0;
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine()); 
+if (number1 > number2) max = number1; 
+else max = number2;
+Console.WriteLine($"Большее число = {max}");

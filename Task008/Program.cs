51cb0cfb4e43b30764ab744fa58071a42a49234f
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-04-27 17:36:46 +0300

    task 8 completed

---
+++ Task008/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine()); 
+int index = 2;
+while (index < n) {
+    Console.Write ($"{index}");
+    index = index + 2;
+    if (index < n) {
+        Console.Write (", ");
+    }
+}
+
+

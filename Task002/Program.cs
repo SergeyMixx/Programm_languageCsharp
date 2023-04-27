fc48f36d2bc111acb572a1239d708edb83f7f93b
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-04-27 16:31:40 +0300

    Completed

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,8 +1,12 @@
-﻿int max =0;
-Console.WriteLine("Введите первое число");
-int number1 = Convert.ToInt32(Console.ReadLine()); 
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int number2 = Convert.ToInt32(Console.ReadLine()); 
-if (number1 > number2) max = number1; 
-else max = number2;
+int max = number1;
+int min = number2;
+if (number1 < number2) {
+    max = number2; 
+    min = number1;
+}
 Console.WriteLine($"Большее число = {max}");
+Console.WriteLine($"Меньшее число = {min}");

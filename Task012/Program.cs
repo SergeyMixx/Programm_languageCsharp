7070d639bebe9ea311c2d2761f8deb08e820f52b
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-05-01 14:23:38 +0300

    Task012 initial commit

---
+++ Task012/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine()); 
+
+int ost = number1 % number2;
+
+if (Divisible (number1, number2) == true)
+{
+Console.WriteLine("кратно");
+}
+else
+{
+Console.WriteLine($"не кратно, остаток {ost}");
+}
+
+bool Divisible (int num1, int num2) 
+{
+    if (num1 % num2 == 0) return true;
+    else return false;
+}
+

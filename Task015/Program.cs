bdc4deb6a380db63556430c257ef8704b9fbc923
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-05-01 16:07:42 +0300

    Task015 completed

--- Task015/Program.cs
+++ Task015/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine()); 
+
+if (number == 6 || number == 7 ) {
+    Console.WriteLine("да");
+}
+
+else {
+    Console.WriteLine("нет");
+}

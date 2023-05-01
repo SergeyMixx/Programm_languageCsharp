532646916013734f6ab4e98cee59110a2d80de90
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-05-01 13:32:58 +0300

    Task011 completed

---
+++ Task011/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = new Random().Next(100, 1000); 
+Console.WriteLine($"Случайное трехзначное чило -> {number}");
+
+int modDigit = ModifyNumber(number);
+Console.WriteLine($"Исправленное число -> {modDigit}");
+
+int ModifyNumber (int num) 
+{
+    int firstDigit = num / 100 * 10;
+    int secondDigit = num % 10;
+    int result = firstDigit + secondDigit;
+    return result;
+}

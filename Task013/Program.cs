6687b5da49dfbd01e8f26ea6635fc95a93350f0c
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-05-01 15:43:21 +0300

    Task013 completed

--- Task013/Program.cs
+++ Task013/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine()); 
+
+if (number < 100) {
+    Console.WriteLine("третьей цифры нет");
+}
+
+else {
+    while (number > 999) {
+    number = number / 10;    
+    }
+    Console.WriteLine($"-> {number % 10}");
+}

2115d57eb0f128b42315bf56f4079e39133847b9
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-05-01 13:28:33 +0300

    initial commit Task011

--- Task009/Program.cs
+++ Task009/Program.cs
@@ -1,14 +1,28 @@
 ﻿int number = new Random().Next(10, 100); //(10, 99 + 1)
 Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
 
-int firstDigit = number / 10;
-int secondDigit = number % 10;
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
 
-if (firstDigit > secondDigit)
+// if (firstDigit > secondDigit)
+// {
+//     Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");    
+// }
+// else
+// {
+//     Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
+// }
+
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int MaxDigit (int num) // num = number, создается копия, поэтому указываем тип переменной
 {
-    Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");    
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;  
 }
-else
-{
-    Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
-}
+

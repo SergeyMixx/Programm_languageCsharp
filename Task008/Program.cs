a85f946ea0580618db243ff535df462d222fe3e7
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-05-01 10:26:51 +0300

    исправлено решение

--- Task008/Program.cs
+++ Task008/Program.cs
@@ -1,10 +1,10 @@
 ﻿Console.WriteLine("Введите число");
 int n = Convert.ToInt32(Console.ReadLine()); 
 int index = 2;
-while (index < n) {
+while (index <= n) {
     Console.Write ($"{index}");
     index = index + 2;
-    if (index < n) {
+    if (index <= n) {
         Console.Write (", ");
     }
 }

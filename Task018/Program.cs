c89f35ef52b72b3c757063547d440b51c4c60da9
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-05-04 23:56:23 +0300

    Task018 initial commit

---
+++ Task018/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите номер четверти");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string coordinates = Coordinates (quarter);
+
+string result = coordinates == null 
+    ?  "Некорректный номер"
+    : $"Указанная четверть соответствует координатам -> {coordinates}";
+Console.WriteLine (result);
+
+string Coordinates (int q)
+{
+    if (q == 1) return "x > 0, y > 0";
+    if (q == 2) return "x < 0, y > 0";
+    if (q == 3) return "x < 0, y < 0";  
+    if (q == 4) return "x > 0, y < 0";
+    return null;  
+}

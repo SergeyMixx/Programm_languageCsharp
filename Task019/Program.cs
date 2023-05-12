c7f89af45655c186457b9b3032d30e615e213f61
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-05-12 19:04:22 +0300

    Task019 completed

--- Task019/Program.cs
+++ Task019/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine()); 
+
+if (polindr (num) == true) Console.WriteLine("да");
+else Console.WriteLine("нет");
+
+bool polindr (int number)
+{
+int numberFirstTwo = number / 1000;
+int numberLastTwo = number % 10 * 10 + number / 10 % 10;
+
+if (numberFirstTwo == numberLastTwo) return true;
+
+else return false;
+}

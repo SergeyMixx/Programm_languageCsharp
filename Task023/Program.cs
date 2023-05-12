14c0da291ad7d1c4c1422caece58447e3dc72990
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-05-12 20:50:42 +0300

    Task023 completed

---
+++ Task023/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите положительное целое число: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+Cube(numN);
+
+void Cube(int num)
+{
+int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count,3} {count * count * count,5}");
+        count++;
+    }
+}

7ff0c05561c91f4f24f3b2cc8f93224bf6890586
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-05-21 15:35:41 +0300

    Task025 completed

---
+++ Task025/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите цикл, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную
+// степень B.
+
+Console.Write("Введите целое число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите натуральное число В: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int degree = Deg (numA, numB);
+
+Console.WriteLine($"Число {numA} в степени {numB} = {degree}");
+
+int Deg(int a, int b)
+{
+    if (b == 0) return 1;
+    else if (b == 1) return a;
+    else
+    {
+        int res = a;
+        for (int i = 2; i <= b; i++)
+        {            
+            res = res * a;
+        }
+        return res;
+    }    
+}

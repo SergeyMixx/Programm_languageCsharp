80235e27026db085a5b9e261380203d6741d0d6f
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-05-21 11:27:55 +0300

    Task024 completed

--- Task024/Program.cs
+++ Task024/Program.cs
@@ -4,3 +4,20 @@
 // 7 -> 28
 // 4 -> 10
 // 8 -> 36
+
+Console.WriteLine("Введите целое положительое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumbers}");
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+

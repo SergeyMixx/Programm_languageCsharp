4cc6d044befceffa07bded09ae1946ff6d35542c
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-05-21 14:22:10 +0300

    Task030 corrected

--- Task030/Program.cs
+++ Task030/Program.cs
@@ -25,7 +25,8 @@ void PrintArray(int[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
     {
-        Console.Write(arr[i] + " ");
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
     }
 Console.WriteLine();
 }

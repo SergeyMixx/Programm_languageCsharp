35ff09eae899f6b1cb9dc30cf29d7ff5b0ac9706
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-05-28 18:00:15 +0300

    Task032 completed

---
+++ Task032/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int[] ChangeNumber (int[] arr)
+{    
+    for (int i = 0; i < arr.Length; i++)
+    {
+       arr[i] = arr[i] * (-1);    
+    }
+    return arr;
+}
+
+int[] array = CreateArrayRndInt(4, -9, 9);
+Console.Write("[");
+PrintArray(array, ",");
+Console.WriteLine("]");
+
+int[] changeNumber = ChangeNumber(array);
+Console.Write("[");
+PrintArray(array, ",");
+Console.WriteLine("]");

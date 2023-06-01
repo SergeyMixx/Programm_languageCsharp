ed2bd6bb3bf17b32d0359f1513a4ff52284376fc
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-06-01 09:33:10 +0300

    Task041 completed

---
+++ Task041/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.Write("Введите количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArrayInt(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите {i + 1}е чисело: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+int PositiveNumbers(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) sum++;
+    }
+    return sum;
+}
+
+int[] array = CreateArrayInt(m);
+int res = PositiveNumbers(array);
+Console.WriteLine($"Количество положительных чисел -> {res}");

fdaf74ec5bc95c6ce34d809044f7917a9a4f537a
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-06-01 18:37:23 +0300

    Task43 completed

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите аргумент k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите аргумент b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите аргумент k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите аргумент b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+double CrossPointCoordinateX(int arg1, int arg2, int arg3, int arg4)
+{
+    double x = (arg4 - arg2) / ((arg1 - arg3) * 1.0);
+    return x;
+}
+
+
+double CrossPointCoordinateY(int arg1, int arg2)
+{
+    double Y = arg1 * CrossPointCoordinateX(k1, b1, k2, b2) + arg2;
+    return Y;
+}
+
+
+double x = CrossPointCoordinateX(k1, b1, k2, b2);
+double y = CrossPointCoordinateY(k1, b1);
+Console.WriteLine($"Координаты точки пересечения -> ({x}; {y})");

67a72235fe663f0e7aa0d7c312f2ea90d7a764a7
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-06-04 18:54:19 +0300

    Task47 completed

---
+++ Task47/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)      // arr.GetLength(0) можно заменить на rows, в скобках номер измерения
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)  // arr.GetLength(1) можно заменить на columns
+        {
+            double num = rnd.NextDouble() * (max - min) + min*1.0;
+            matrix[i, j] = Math.Round(num, 1);
+        }       
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix) 
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 5} ");    
+        }  
+       Console.WriteLine();
+       
+    }
+}
+
+
+double[,] array2D = CreateMatrixRndInt(3, 4, -10, 10);
+PrintMatrix(array2D);

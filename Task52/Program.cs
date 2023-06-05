c60bad9a9dbe4c3473a0deb7a65a4f4a6688a59a
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-06-05 18:09:11 +0300

    Task50 corrected

---
+++ Task52/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)      // arr.GetLength(0) можно заменить на rows, в скобках номер измерения
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)  // arr.GetLength(1) можно заменить на columns
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }       
+    }
+    return matrix;
+}

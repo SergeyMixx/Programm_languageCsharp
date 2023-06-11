c6c3cd701cf793997233c08a075758014d96ab83
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-06-11 19:03:47 +0300

    task65 completed

--- Task65/Program.cs
+++ Task65/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите натуральное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите натуральное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(m, n);
+
+void NaturalNumbers(int num1, int num2)
+{
+    if (num1 > num2)
+    {
+        Console.Write($"{num1} ");
+        NaturalNumbers(num1 - 1, num2);
+       
+    }
+    else if (num1 < num2)
+    {
+        Console.Write($" {num2} ");
+        NaturalNumbers(num1, num2 - 1);        
+    }
+    else Console.Write($" {num1} ");
+}

8ac4b0650000f02652f9377a933630f1b9e6b0d7
Sergey Mikhaylov <sergey.a.mikhaylov@gmail.com>
2023-06-12 11:20:21 +0300

    Task68 completed

---
+++ Task66/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите натуральное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите натуральное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+bool CheckIfNumberNatural(int m, int n)
+{
+    if (n > 0 && m > 0) return true;
+    else return false;
+}
+
+if (CheckIfNumberNatural(m, n) == true)
+{
+    int res = NaturalNumbers(m, n);
+    Console.WriteLine($"Сумма чисел -> {res}");
+}
+else Console.WriteLine("Число не натуральное");
+
+int NaturalNumbers(int num1, int num2)
+{
+    if (num1 > num2)
+    {        
+        return num1 + NaturalNumbers(num1 - 1, num2);       
+    }
+    
+    else if (num1 < num2)
+    {
+        return num2 + NaturalNumbers(num1, num2 - 1);        
+    }
+    else return num1;
+}    
---
+++ Task68/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите неотрицательное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите неотрицательное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+bool CheckIfNumberPositive(int m, int n)
+{
+    if (n >= 0 && m >= 0) return true;
+    else return false;
+}
+
+if (CheckIfNumberPositive(m, n) == true)
+{
+    int res = Ack(m, n);
+    Console.WriteLine($"{res}");
+}
+else Console.WriteLine("Число отрицательное");
+
+int Ack(int num1, int num2)
+{
+    if (num1 == 0) return (num2 + 1);
+    else if (num2 == 0) return Ack(num1 - 1, 1);
+    else return Ack(num1 - 1, Ack(num1, num2 - 1));
+}
